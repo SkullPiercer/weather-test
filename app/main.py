18ba42f48fe245661823e81c9549f637c4ff9c5b
Sasha <akatsuki_sanya@vk.com>
2025-10-30 16:24:01 +0600

    remade 2

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import os
 import uvicorn
 from fastapi import FastAPI
 
@@ -11,5 +12,6 @@ settings = get_settings()
 app = FastAPI(title=settings.APP_TITLE, description=settings.APP_DESCRIPTION)
 app.include_router(main_router)
 
-if __name__ == '__main__':
-    uvicorn.run('app.main:app', reload=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
