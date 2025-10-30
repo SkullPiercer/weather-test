5429a3ff230de22ea286528d035cb9d6c09dbd05
Sasha <akatsuki_sanya@vk.com>
2025-10-30 16:14:46 +0600

    Remade

--- app/core/config.py
+++ app/core/config.py
@@ -7,12 +7,6 @@ class Settings(BaseSettings):
     APP_DESCRIPTION: str
     WEATHER_API_KEY: str
 
-    DB_USER: str
-    DB_PASSWORD: str
-    DB_NAME: str
-    DB_PORT: int
-    DB_HOST: str
-
     @property
     def DB_URL(self):
         return f'sqlite+aiosqlite:///./fastapi.db'
